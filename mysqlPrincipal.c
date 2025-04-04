318fdfa36b080e65f2bdb7be2d15834cd53e9a57
Atom M. Nava <atommunoznava@gmail.com>
2025-04-04 14:18:40 -0600

    Update mysqlPrincipal.c

--- mysqlPrincipal.c
+++ mysqlPrincipal.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-void menu_principal()
+void menu_principal(void)
 {
 printf( "                                                                                                                                 ,----,                                       \n");
 printf( "                                                 ,----..                ____                                                   ,/   .`|     ,----..                           \n");
