22aae7b6ce66d30bf5fb5fca9f625813bd7620ed
Atom M. Nava <atommunoznava@gmail.com>
2025-04-04 14:18:18 -0600

    Subí el archivo principal e hice la función del menu.

--- mysqlPrincipal.c
+++ mysqlPrincipal.c
@@ -3,18 +3,50 @@
 #include <string.h>
 #include <stdlib.h>
 
+void menu_principal()
+{
+printf( "                                                                                                                                 ,----,                                       \n");
+printf( "                                                 ,----..                ____                                                   ,/   .`|     ,----..                           \n");
+printf( "    ,---,     ,---,.      ,---,. ,-.----.       /   /                 ,'  , `.     ,---,.  ,--,     ,--,      .--.--.        ,`   .'  :    /   /   v   ,-.----.        ,---,. \n");
+printf( " ,`--.' |   ,'  .'      ,'  .' |      /        /   .     :         ,-+-,.' _ |   ,'  .' |  |'. v   / .`|     /  /    '.    ;    ;     /   /   .     :  v    /  v     ,'  .' | \n");
+printf( " |   :  : ,---.' .' | ,---.'   | ;   :        .   /   ;.        ,-+-. ;   , || ,---.'   |  ; v `v /' / ;    |  :  /`. /  .'___,/    ,'   .   /   ;.  v ;   :    v  ,---.'   | \n");
+printf( " :   |  ' |   |  |: | |   |   .' |   | .  :  .   ;   /  ` ;    ,--.'|'   |  ;| |   |   .'  `. v  /  / .'    ;  |  |--`   |    :     |   .   ;   /  ` ; |   | .v :  |   |   .' \n");
+printf( " |   :  | :   :  :  / :   :  |-, .   : |: |  ;   |  ;   ; |   |   |  ,', |  ': :   :  |-,   v  vv  / ./     |  :  ;_     ;    |.';  ;   ;   |  ; v ; | .   : |: |  :   :  |-, \n");
+printf( " '   '  ; :   |    ;  :   |  ;/| |   |    :  |   :  | ; | '   |   | /  | |  || :   |  ;/|    v  vv'  /       v  v    `.  `----'  |  |   |   :  | ; | ' |   |  v :  :   |  ;/| \n");
+printf( " |   |  | |   :       |   :   .' |   : .  /  .   |  ' ' ' :   '   | :  | :  |, |   :   .'     v  ;  ;         `----.   v     '   :  ;   .   |  ' ' ' : |   : .  /  |   :   .' \n");
+printf( " '   :  ; |   |   . | |   |  |-, ;   | |     '   ;     /  |   ;   . |  ; |--'  |   |  |-,    / v  v  v        __ v  v  |     |   |  '   '   ;  vv /  | ;   | |  v  |   |  |-, \n");
+printf( " |   |  ' '   :  '; | '   :  ;/| |   | ;             ',  /    |   : |  | ,     '   :  ;/|   ;  /v  v  v      /  /`--'  /     '   :  |    v   v  ',  /  |   | ;v  v '   :  ;/| \n");
+printf( " '   :  | |   |  | ;  |   |      :   ' |   '   ;   :    /     |   : '  |/      |   |    v ./__;  v  ;  v    '--'.     /      ;   |.'      ;   :    /   :   ' | vv' |   |    v \n");
+printf( " ;   |.'  |   :   /   |   :   .' :   : :-'            .'      ;   | |`-'       |   :   .' |   : / v  v  ;     `--'---'       '---'         v   v .'    :   : :-'   |   :   .' \n");
+printf( " '---'    |   | ,'    |   | ,'   |   |.'         `---`        |   ;/           |   | ,'   ;   |/   v  ' |                                   `---`      |   |.'     |   | ,'   \n");
+printf( "          `----'      `----'     `---'                        '---'            `----'     `---'     `--`                                               `---'       `----'     \n");
+
+  printf("BIENVENID@  A NUESTRA TIENDA DEPARTAMENTAL!\n");
+  printf("OPCIONES: \n");
+  printf("1. Clientes que más han comprado\n");
+  printf("2. Posibilidad de calcular la facturación total de un cliente en particular.\n");
+  printf("3 Lista de los clientes que no han comprado ningún producto. \n");
+  printf("4. Lista de todos los clientes que han comprado productos por más de una cantidad específica, proporcionada por el usuario.\n");
+  printf("5. Lista de cada compra, nombre del cliente, número de productos y descripción de cada uno de los productos.\n");
+  printf("6. Lista de asesores que han resuelto los problemas satisfactoriamente.\n");
+  printf("7 Lista de asesores que tienen todavía casos abiertos.\n");
+  printf("8 Lista de todos los productos de una categoría en particular, mostrando su nombre, descripción y comentarios (en su caso) de clientes que hayan opinado acerca de dicho producto. \n");
+  printf("9. Cuántas cancelaciones y devoluciones se han hecho y cuál es el producto más devuelto.\n");
+  printf("10. Lista de los clientes con su información completa para poder hacer una entrega.\n");
+}
+
 int main (int argc, char **argv)
 {
     unsigned int i = 0;
     char     buffer[1024];
     unsigned int num_fields;
 
-        // Se cambian los valores de estas variables dependiendo
-        // del usuario que se conectará
-
+	// Se cambian los valores de estas variables dependiendo
+	// del usuario que se conectará
+	
     char *server = "localhost";
-    char *user = "usuario";
-    char *passwd = "contraseña";
+    char *user = "cusuario";
+    char *passwd = "password";
     char *db = "usuario";
 
     char nombre[10];
@@ -26,8 +58,10 @@ int main (int argc, char **argv)
     MYSQL_ROW row;
     MYSQL_FIELD *fields;
 
+
     if( argc != 3 ){
       fputs("Usage: mysql <dataBase> <\"query\">\n", stderr);
+      menu_principal();
       return 1;
     }
 
@@ -63,18 +97,18 @@ int main (int argc, char **argv)
         i = 0;
 
         for( i=0 ; i < mysql_num_fields(res); i++ ){
-          if(row[i] != NULL){
+	  if(row[i] != NULL){
             printf("%s ",row[i]);
-          }
-          else{
-            printf(" \n");
-          }
+	  }
+	  else{
+	    printf(" \n");
+	  }
         }
         fputc('\n',stdout);
     }
 
     mysql_free_result(res);
-
+    
     // cierra la conexion con al servidor
     mysql_close(&mysql);
 
